1be941650ca2c2cb04ea95950be41e254fc38a92
Rana Alshaikh <rmubarakalshaikh@stu.kau.edu.sa>
2023-09-13 20:17:22 +0300

    First commit

--- src/cpit251_project/CPIT251_Project.java
+++ src/cpit251_project/CPIT251_Project.java
@@ -5,7 +5,7 @@ package cpit251_project;
 public class CPIT251_Project {
 
     public static void main(String[] args) {
-      
+      System.out.println("Software Engineering Project");
     }
     
 }
